050cc6b9b0a97e8af5102541c010fcab37b1ba78
Ghazanfar-ijaz <ghazanfar@outlook.fr>
2021-03-26 17:14:09 +0100

    ok

--- threeWayLoop.js
+++ threeWayLoop.js
@@ -1,19 +1,15 @@
 const tab = ['Alice', 'Bob', 'Charlie', 'Dan', 'Eve']
 
 
-
-// V1
 for (let i = -1; i < tab.length; i++) {
   console.log(`${tab[i]}, length: ${tab[i].length}`)
 }
 
-/*
 for (const elem of tab) {
   console.log(`${elem}, length: ${elem.length}`)
 }
 
-// V3
+
 tab.forEach((elem) => {
   console.log(`${elem}, length: ${elem.length}`)
 })
-*/
